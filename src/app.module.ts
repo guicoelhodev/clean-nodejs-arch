6d0d2b59eea2943e7006eba46634405ef6142156
guicoelhodev <gs.coelho_dev@outlook.com>
2024-12-11 10:30:54 +0000

    feat: create Users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvConfigModules } from './shared/infrastructure/env-config/env-config.module';
+import { UsersModule } from './users/infrastructure/users.module';
 
 @Module({
-  imports: [EnvConfigModules],
+  imports: [EnvConfigModules, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
